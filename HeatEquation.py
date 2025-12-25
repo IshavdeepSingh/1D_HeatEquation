ec9d53a7b35fc5dfa87911e97e0ea44a9ee3e9ae
IshavdeepSingh <145477825+IshavdeepSingh@users.noreply.github.com>
2025-12-25 15:59:00 -0500

    Initialization

--- HeatEquation.py
+++ HeatEquation.py
@@ -9,7 +9,19 @@ import matplotlib.pyplot as plt
 
 # first we define a single 1d rod to model our equation 
 
-a = 110 # mm^2/2
-length = 50 # mm
-time = 4 #seconds 
+a = 110 # mm^2/2 this is thermal diffusability, unit of how fast 
+length = 50 # mm length of the rod
+time = 4 #seconds time taken for 
+nodes = 10
+
+# initialization 
+dx = lenth / nodes #spatial discrete element basically the disance between the nodes
+dt = 0.5 * dx**2 / a # derived from the stability analysis of the finite difference scheme ensuring the numerical solution remains stable
+t_nodes = int(time/dt) 
+
+# the temperature at the ends of the rod must be known at any time 
+#distribution before starting the simulation must also be known 
+
+u = np.zeroes(nodes) + 20 # we keep the plate initially at 20 degrees 
+
 
