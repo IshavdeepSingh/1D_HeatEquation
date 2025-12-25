7cb0068e26e985cc77353e9019450b0507986274
IshavdeepSingh <145477825+IshavdeepSingh@users.noreply.github.com>
2025-12-25 15:24:29 -0500

    Creating a simulation for the heat equation

---
+++ HeatEquation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# first we define a single 1d rod to model our equation 
+
+a = 110 # mm^2/2
+length = 50 # mm
+time = 4 #seconds 
+
